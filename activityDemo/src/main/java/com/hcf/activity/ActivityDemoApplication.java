863fc261987a01315aad1bfe0c08d591a9747457
Chuanfei.he <Chuanfei.he@CD62235PCW.spreadtrum.com>
2019-12-05 15:34:26 +0800

    # WARNING: head commit changed in the meantime
    3

--- activityDemo/src/main/java/com/hcf/activity/ActivityDemoApplication.java
+++ activityDemo/src/main/java/com/hcf/activity/ActivityDemoApplication.java
@@ -3,7 +3,6 @@ package com.hcf.activity;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
