3735d7c86d722c6f66b7a07772ba2b9303b01ed5
hechuanfei <846656938@qq.com>
2019-12-10 18:14:01 +0800

    增加MVC支持

--- activityDemo/src/main/java/com/hcf/activity/config/mvc/MvcConfigure.java
+++ activityDemo/src/main/java/com/hcf/activity/config/mvc/MvcConfigure.java
@@ -1,32 +1,47 @@
 package com.hcf.activity.config.mvc;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class MvcConfigure  extends WebMvcConfigurationSupport {
+public class MvcConfigure extends WebMvcConfigurationSupport {
+
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/static/");// 静态资源路径																					// css,js,img等
+		registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");// 静态资源路径css,js,img等
 		registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");// 视图
 		registry.addResourceHandler("/mapper/**").addResourceLocations("classpath:/mapper/");// mapper.xml
 		super.addResourceHandlers(registry);
 	}
 
-//	public void addCorsMappings(CorsRegistry registry) {
-//		// 匹配所有的URI，允许所有的外域发起跨域请求，允许外域发起请求POST/GET，允许跨域请求包含任意的头信息。
-//		registry.addMapping("/**").allowedOrigins("*").allowedMethods("POST", "GET").allowedHeaders("*")
-//				.allowCredentials(true);
-//	}
-//
-//	public void addFormatters(FormatterRegistry registry) {
-//		registry.addFormatter(new DateFormatter("yyyy-MM-dd HH:mm:ss"));
-//	}
-//
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		// 将/index重定向到/index.html
-//		registry.addRedirectViewController("/index", "/index.html");
-//	}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")// 配置允许跨域的路径
+				.allowedOrigins("*")// 配置允许访问的跨域资源的请求域名
+				.allowedMethods("PUT,POST,GET,DELETE,OPTIONS")// 配置允许访问该跨域资源服务器的请求方法，如：POST、GET、PUT、DELETE等
+				.allowedHeaders("*"); // 配置允许请求header的访问，如 ：X-TOKEN
+		super.addCorsMappings(registry);
+	}
+
+	//
+	// public void addFormatters(FormatterRegistry registry) {
+	// registry.addFormatter(new DateFormatter("yyyy-MM-dd HH:mm:ss"));
+	// }
+	//
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("/index");// 默认视图跳转
+		registry.addViewController("/index").setViewName("/index");
+		registry.addViewController("/article").setViewName("/article");
+		registry.addViewController("/error/404").setViewName("/error/404");
+		registry.addViewController("/error/500").setViewName("/error/500");
+		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		super.addViewControllers(registry);
+	}
+
 }
